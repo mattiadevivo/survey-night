7dbc09c3d5ec5864d7efe6b4219a2da3ead411ae
Mattia De Vivo <mattia.devivo@igenius.ai>
2023-06-28 10:18:08 +0200

    change: move contact form to TSX

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,14 +1,22 @@
 import { useState } from 'react';
 import { sendForm } from '@emailjs/browser';
+import { ChevronDownIcon } from '@heroicons/react/20/solid';
 
 export default function Form() {
   const [responseMessage, setResponseMessage] = useState('');
 
-  async function submit(e: SubmitEvent) {
+  async function handleSubmit(e: React.SyntheticEvent<HTMLFormElement>) {
     e.preventDefault();
+    e.stopPropagation();
     const formData = new FormData(e.target as HTMLFormElement);
+    console.log(formData);
+    console.log(e.target);
     try {
-      await sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', '#myForm');
+      await sendForm(
+        'YOUR_SERVICE_ID',
+        'YOUR_TEMPLATE_ID',
+        e.target as HTMLFormElement,
+      );
       setResponseMessage('ok');
     } catch (error) {
       console.log(error);
@@ -16,21 +24,137 @@ export default function Form() {
   }
 
   return (
-    <form onSubmit={submit}>
-      <label>
-        Name
-        <input type="text" id="name" name="name" required />
-      </label>
-      <label>
-        Email
-        <input type="email" id="email" name="email" required />
-      </label>
-      <label>
-        Message
-        <textarea id="message" name="message" required />
-      </label>
-      <button>Send</button>
-      {responseMessage && <p>{responseMessage}</p>}
+    <form className="mx-auto mt-16 max-w-xl sm:mt-20" onSubmit={handleSubmit}>
+      <div className="grid grid-cols-1 gap-y-6 gap-x-8 sm:grid-cols-2">
+        <div>
+          <label
+            htmlFor="first-name"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            First name
+          </label>
+          <div className="mt-2.5">
+            <input
+              type="text"
+              name="first-name"
+              id="first-name"
+              autoComplete="given-name"
+              className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
+          <label
+            htmlFor="last-name"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            Last name
+          </label>
+          <div className="mt-2.5">
+            <input
+              type="text"
+              name="last-name"
+              id="last-name"
+              autoComplete="family-name"
+              className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="company"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            Company
+          </label>
+          <div className="mt-2.5">
+            <input
+              type="text"
+              name="company"
+              id="company"
+              autoComplete="organization"
+              className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="email"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            Email
+          </label>
+          <div className="mt-2.5">
+            <input
+              type="email"
+              name="email"
+              id="email"
+              autoComplete="email"
+              className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="phone-number"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            Phone number
+          </label>
+          <div className="relative mt-2.5">
+            <div className="absolute inset-y-0 left-0 flex items-center">
+              <label htmlFor="country" className="sr-only">
+                {' '}
+                Country Prefix
+              </label>
+              <select
+                id="country"
+                name="country"
+                className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 pr-9 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+              >
+                <option>+39</option>
+              </select>
+              <ChevronDownIcon
+                className="pointer-events-none absolute top-0 right-3 h-full w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </div>
+            <input
+              type="tel"
+              name="phone-number"
+              id="phone-number"
+              autoComplete="tel"
+              className="block w-full rounded-md border-0 py-2 px-3.5 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div className="sm:col-span-2">
+          <label
+            htmlFor="message"
+            className="block text-sm font-semibold leading-6 text-gray-900"
+          >
+            Message
+          </label>
+          <div className="mt-2.5">
+            <textarea
+              name="message"
+              id="message"
+              rows={4}
+              className="block w-full rounded-md border-0 py-2 px-3.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            ></textarea>
+          </div>
+        </div>
+        <div className="mt-10">
+          <button
+            type="submit"
+            id="contact-form-submit"
+            className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Let's talk
+          </button>
+        </div>
+        {responseMessage && <p>{responseMessage}</p>}
+      </div>
     </form>
   );
 }
