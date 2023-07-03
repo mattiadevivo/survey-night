4dea1ed5ba4795670d0910bde1d0882156fc7dec
Mattia De Vivo <mattia.devivo@igenius.ai>
2023-07-03 09:21:22 +0200

    add: github stuff for deploying the site

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,23 +1,24 @@
 import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'static',
+  site: 'https://www.surveynight.com',
   // if 使用 docker, 请将 adapter 切换到 node
   // adapter: node({
   //   mode: 'standalone'
   // }),
   adapter: vercel({
-    analytics: true
+    analytics: true,
   }),
   integrations: [react(), tailwind()],
   markdown: {
     // Example: Switch to use prism for syntax highlighting in Markdown
-    syntaxHighlight: 'prism'
-  }
+    syntaxHighlight: 'prism',
+  },
 });
